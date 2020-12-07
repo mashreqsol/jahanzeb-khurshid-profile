4211e1a1d68a90f73b983b3ee0d74b17ed803b6c
mashreqsol <47236439+mashreqsol@users.noreply.github.com>
2020-12-07 15:47:43 +0500

    favicon update with company logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Metadata from "../components/metadata"
 const Index = () => {
   return (
     <Layout>
-      <Metadata title="Home" description="This is my home page of Jahanzeb Khurshid site." />
+      <Metadata title="Mashreq Solutions" description="Mashreq Solutions Website" />
       <h1>Home page</h1>
       <h2>I'm Jahanzeb Khurshid, eager to learn as much as possible... Recent Technologies</h2>
     </Layout>

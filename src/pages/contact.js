86a5643dad119e83770bd355df686beb61728dfe
mashreqsol <47236439+mashreqsol@users.noreply.github.com>
2020-12-31 22:58:04 +0500

    Contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,6 +6,7 @@ const ContactFormPage = () => (
     <h1>Contact</h1>
 
     <form name="contact" method="POST" data-netlify="true" action="/thank-you/">
+    <input type="hidden" name="form-name" value="contact" />
   <p>
     <label>Your Name: <input type="text" name="name" /></label>   
   </p>
